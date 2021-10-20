38f525fb7ea42185103c7ee983a6dde1d75d215b
michaelstrefeler <mstrefeler@me.com>
2021-10-20 13:51:15 +0200

    Fixed 14 part 1. Added 4 functions instead

--- Labo-014/strefeler_labo14_1.cpp
+++ Labo-014/strefeler_labo14_1.cpp
@@ -1,5 +1,5 @@
 /*
-    But: afficher le résultat d'une série harmonique de taille k
+    But: Afficher le résultat d'une série harmonique de taille k de 4 manières
     Nom du fichier: strefeler_labo14_1.cpp
     Auteur: Michael Strefeler
     Date de création: 19.10.21
@@ -8,10 +8,53 @@
 
 using namespace std;
 
+// Série harmonique de 1/1 + ... 1/k en float
+float harmonic_float_asc(int k)
+{
+    float sum = 0.0f;
+    for (int i = 1; i <= k; ++i)
+    {
+        sum += 1.0f / float(i);
+    }
+    return sum;
+}
+
+// Série harmonique de 1/k + 1/k-1 ... 1/1 en float
+float harmonic_float_desc(int k)
+{
+    float sum = 0.0f;
+    for (int i = k; i > 0; --i)
+    {
+        sum += 1.0f / float(i);
+    }
+    return sum;
+}
+
+// Série harmonique de 1/1 + ... 1/k en double
+double harmonic_double_asc(int k)
+{
+    double sum = 0.0;
+    for (int i = 1; i <= k; ++i)
+    {
+        sum += 1.0 / i;
+    }
+    return sum;
+}
+
+// Série harmonique de 1/k + 1/k-1 ... 1/1 en double
+double harmonic_double_desc(int k)
+{
+    double sum = 0.0;
+    for (int i = k; i > 0; --i)
+    {
+        sum += 1.0 / i;
+    }
+    return sum;
+}
+
 int main()
 {
     int k;
-    double s = 0.0;
     cout << "Combien de termes voulez-vous ? ";
     cin >> k;
     while(k <= 0){
@@ -19,9 +62,13 @@ int main()
         "\nCombien de termes voulez-vous ? ";
         cin >> k;
     }
-
-    for(int i = k; i > 0; --i){
-        s += 1.0/i;
-    }
-    cout << "La somme des "<< k << " premiers termes de la serie vaut " << s;
+    // Affiche tous les résultats
+    cout << "\nLa somme des " << k << " premiers termes de la serie vaut "
+         << harmonic_float_asc(k) << " Float ascendant"
+         << "\nLa somme des " << k << " premiers termes de la serie vaut "
+         << harmonic_float_desc(k) << " Float descendant"
+         << "\nLa somme des " << k << " premiers termes de la serie vaut "
+         << harmonic_double_asc(k) << " double ascendant"
+         << "\nLa somme des " << k << " premiers termes de la serie vaut "
+         << harmonic_double_desc(k) << " double descendant";
 }
