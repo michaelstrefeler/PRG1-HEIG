c349c42ef9ae6fedc3cc2968da2def3eb8584161
michaelstrefeler <mstrefeler@me.com>
2021-10-20 10:25:05 +0200

    Finished part 1 of lab 014

--- Labo-014/strefeler_labo14_1.cpp
+++ Labo-014/strefeler_labo14_1.cpp
@@ -1,5 +1,5 @@
 /*
-    But: 
+    But: afficher le résultat d'une série harmonique de taille k
     Nom du fichier: strefeler_labo14_1.cpp
     Auteur: Michael Strefeler
     Date de création: 19.10.21
@@ -11,7 +11,17 @@ using namespace std;
 int main()
 {
     int k;
+    double s = 0.0;
     cout << "Combien de termes voulez-vous ? ";
     cin >> k;
-    
+    while(k <= 0){
+        cerr << "\nErreur. La valeur saisie doit etre > 0 "<<
+        "\nCombien de termes voulez-vous ? ";
+        cin >> k;
+    }
+
+    for(int i = k; i > 0; --i){
+        s += 1.0/i;
+    }
+    cout << "La somme des "<< k << " premiers termes de la serie vaut " << s;
 }
