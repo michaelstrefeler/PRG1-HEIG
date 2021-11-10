1ce565110f419ddefb29b9c17844cad1931aa199
michaelstrefeler <mstrefeler@me.com>
2021-11-10 11:35:03 +0100

    Added working deleteMiddle function

--- Labo-019/strefeler_labo19.cpp
+++ Labo-019/strefeler_labo19.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 /**
  * @brief Move all elements of an array to the right
- * 
+ *
  * @param array array used
  * @param array_size size of the array
  */
@@ -23,20 +23,74 @@ void moveRight(int array[], size_t array_size)
 {
     int right = array[array_size - 1];
 
-    for (size_t i = array_size - 1; i > 0; --i){
+    for (size_t i = array_size - 1; i > 0; --i)
+    {
         array[i] = array[i - 1];
     }
     array[0] = right;
 
+    cout << "Move right result:" << endl;
     for (size_t i = 0; i < array_size; ++i)
     {
         cout << array[i] << " ";
     }
+    cout << endl;
+}
+
+/**
+ * @brief Deletes middle cell of array or middle 2 cells if the size is even
+ *
+ * @param array array that will be modified
+ * @param array_size size of the array
+ */
+void DeleteMiddle(int array[], size_t &array_size)
+{
+    size_t position;
+    if (array_size % 2 == 0)
+    {
+        position = (array_size / 2) - 1;
+        if (position < array_size)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (size_t i = position + 1; i < array_size; ++i)
+                {
+                    array[i - 1] = array[i];
+                }
+                --array_size;
+            }
+        }
+    }
+    else
+    {
+        position = (array_size / 2);
+        if (position < array_size)
+        {
+            for (size_t i = position + 1; i < array_size; ++i)
+            {
+                array[i - 1] = array[i];
+            }
+            --array_size;
+        }
+    }
+
+    cout << "Delete middle result:" << endl;
+    for (size_t i = 0; i < array_size; ++i)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
 {
-    int array[] = {0, 1, 2, 3, 4, 5};
-    size_t tab_size = sizeof(array) / sizeof(array[0]);
-    moveRight(array, tab_size);
+    int array_one[] = {0, 1, 2, 3, 4, 5};
+    const size_t CAPACITY_ONE = sizeof(array_one) / sizeof(int);
+    size_t size_one = CAPACITY_ONE;
+    int array_two[] = {0, 1, 2, 3, 4, 5, 6};
+    const size_t CAPACITY_TWO = sizeof(array_two) / sizeof(int);
+    size_t size_two = CAPACITY_TWO;
+    moveRight(array_one, size_one);
+    DeleteMiddle(array_one, size_one);
+    DeleteMiddle(array_two, size_two);
 }
