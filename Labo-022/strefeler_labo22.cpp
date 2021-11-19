76c7272d31e869c28ec2a209125d57cdf6cd849e
michaelstrefeler <mstrefeler@me.com>
2021-11-19 11:58:08 +0100

    Add method works need to do the fibonacci part

---
+++ Labo-022/strefeler_labo22.cpp
@@ -0,0 +1,74 @@
+/**
+ * @file strefeler_labo22.cpp
+ * @author Michael Strefeler
+ * @brief Learn how to use vector methods and learn about Fibonacci
+ * @version 0.1
+ * @date 19-11-2021
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> add(vector<int> v1, vector<int> v2)
+{
+    while (v1.size() > v2.size())
+    {
+        v2.insert(v2.begin(), 0);
+    }
+
+    while (v2.size() > v1.size())
+    {
+        v1.insert(v1.begin(), 0);
+    }
+
+    size_t size = v1.size();
+    vector<int> sum;
+    sum.resize(size);
+    int temp = 0;
+    for (size_t i = size; i > 0; --i)
+    {
+        sum.at(i - 1) = v1.at(i - 1) + v2.at(i - 1) + temp;
+        temp = 0;
+        if (sum.at(i - 1) > 9 and i != 1 and i!=0)
+        {
+            temp = sum.at(i - 1) / 10;
+            sum.at(i - 1) %= 10;
+        }
+        if(i == 1){
+            if (sum.at(0) > 9)
+            {
+                int carry = sum.at(0) / 10;
+                sum.insert(sum.begin(), carry);
+                sum.at(0) %= 10;
+                sum.erase(sum.begin());
+            }
+        }
+    }
+    return sum;
+}
+
+int fibonacci_n(vector<int> v)
+{
+    return v[0];
+}
+
+int main()
+{
+    vector<int> fibonacci = {0, 1, 1};
+    int number;
+    cout << "Give me a number and I'll tell you the nth number of the Fibonacci sequence: ";
+    cin >> number;
+    vector<int> v1 = {1};
+    vector<int> v2 = {1};
+    if(number == 0 or number == 1){
+        cout << "0 is the first element in the sequence" << endl;
+        return EXIT_SUCCESS;
+    }
+    for(int i = 0; i < number; ++i){
+        
+    }
+}
