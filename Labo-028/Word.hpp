de0a7f01962390a7b55d0a6d9dcd3e4c41abf49c
michaelstrefeler <mstrefeler@me.com>
2021-12-07 08:36:40 +0100

    Added files for future lab

---
+++ Labo-028/Word.hpp
@@ -0,0 +1,12 @@
+#ifndef WORD_HPP
+#define WORD_HPP
+/**
+ * @brief Word in a text with it's occurences
+ *
+ */
+class Word
+{
+    // Need to put stuff here
+};
+
+#endif
---
+++ Labo-028/strefeler_labo28.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file strefeler_labo28.cpp
+ * @author Michael Strefeler
+ * @brief Use a Word class to count how many words are in a text
+ * @version 0.1
+ * @date 07-12-2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
