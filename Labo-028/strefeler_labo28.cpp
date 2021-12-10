29d4f9d6a70cf0a8a60f43c7da28bace4e6d7546
michaelstrefeler <mstrefeler@me.com>
2021-12-10 08:54:23 +0100

    Modified somebody else's file

--- Labo-028/strefeler_labo28.cpp
+++ Labo-028/strefeler_labo28.cpp
@@ -1,110 +1,196 @@
 /**
  * @file strefeler_labo28.cpp
  * @author Michael Strefeler
- * @brief Use a Word class to count how many words are in a text
+ * @brief Opens text file and makes a list of all of the words in order of most used
  * @version 0.1
- * @date 07-12-2021
+ * @date 10-12-2021
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-
-#include "Word.hpp"
-#include <iostream>
-#include <string>
-#include <iterator>
-#include <vector>
 #include <algorithm>
 #include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-/**
- * @brief Spearates text into words and puts the words in the vector
- *
- * @param words vector of words
- * @param text text to use
- */
-void put_words_in_vector(vector<string> &words, string text)
+class Word
 {
-    string word;
-    for (size_t i = 0; i < text.length(); ++i)
+private:
+    string text;
+    int count;
+
+public:
+    Word(const string &initWord = "", int initCount = 1);
+    string getText() const;
+    void setText(const string &setWord);
+    int getCount() const;
+    void setCount(int setCount);
+    bool operator<(Word const &w) const;
+    bool operator!=(Word const &w) const;
+};
+
+Word::Word(const string &initWord, int initCount) : text(initWord), count(initCount) {}
+
+string Word::getText() const
+{
+    return text;
+}
+
+void Word::setText(const string &setWord)
+{
+    text = setWord;
+}
+
+void Word::setCount(int setCount)
+{
+    count = setCount;
+}
+
+int Word::getCount() const
+{
+    return count;
+}
+
+bool Word::operator<(Word const &w) const
+{
+    return text < w.getText();
+}
+
+bool Word::operator!=(Word const &w) const
+{
+    return text != w.getText();
+}
+
+/** @brief Parses a string, separates it's words and put them in a vector
+ * @details Does not take the punctuation, spaces and non-printable char
+ * @param text String that contains the words to separate
+ * @return The vector filled by separated words */
+vector<Word> separateWords(string &text);
+
+/** @brief Counts occurences of a Word in a vector<Word> and erase them
+ * @details Sort the vector by count and lexicograpgically
+ * @param v The vector to be counted
+ * @return vector<Word> without any occurences*/
+vector<Word> countWordOccurences(vector<Word> &v);
+
+/** @brief Outputs a formatted vector<Words> in the std:cout stream
+ * @param w The vector*/
+void outputText(const vector<Word> &w);
+
+/** @brief Compares 2 words to be sorted by occurence and lexicographically
+ * @details Used as predicate for the sort function
+ * @param w1 The first word
+ * @param w2 The second word
+ * @return true if w1 < w2 in terms of predicate */
+bool compareOccurences(const Word &a, const Word &b);
+
+int main()
+{
+    string text;
+
+    // Reads the file
+    getline(ifstream("la_comedie_humaine.txt"), text, '\0');
+
+    // Separate all words and put it in a vector
+    vector<Word> words = separateWords(text);
+
+    // Count occurences, delete them, sort the vector by count and output it to a file
+    outputText(countWordOccurences(words));
+}
+
+bool compareOccurences(const Word &w1, const Word &w2)
+{
+    // Sort lexicographically if counts are identical
+    if (w1.getCount() == w2.getCount())
+        return w1.getText() < w2.getText();
+
+    // Sort by count
+    return w1.getCount() > w2.getCount();
+}
+
+void outputText(const vector<Word> &w)
+{
+    ofstream output_file("output.txt");
+
+    for (auto i = w.begin(); i != w.end(); ++i)
     {
-        if (isalpha(text.at(i)))
+        // Write to the file
+        output_file << (*i).getText() << " : " << (*i).getCount() << endl;
+    }
+    // Close the file
+    output_file.close();
+}
+
+vector<Word> countWordOccurences(vector<Word> &w)
+{
+    // Sort the vector lexicographically
+    sort(w.begin(), w.end());
+
+    vector<Word> sortedVector;
+    int cnt = 1;
+    for (auto i = w.begin(); i != w.end(); ++i)
+    {
+        // Checks if the next word is different
+        if (*i != *(i + 1))
         {
-            word.push_back(text.at(i));
+            // Puts the count in the last Word object
+            (*i).setCount(cnt);
+
+            // Push it back in the sorted vector
+            sortedVector.push_back(*i);
+
+            // Reset the count
+            cnt = 1;
         }
         else
-        {
-            // Accent support
-            if (text.at(i) == '\xC3' or text.at(i) == '\xC5' or text.at(i) == '\xC2')
-            {
-                word.push_back(text.at(i));
-                word.push_back(text.at(i + 1));
-                ++i;
-            }
-            else if (text.at(i) == '\xE2') // Unwanted characters
-            {
-                i += 2;
-                if (word != "")
-                {
-                    words.push_back(word);
-                }
-                word.clear();
-            }
-            else
-            {
-                // Add word to vector unless it's a blank string
-                if (word != "")
-                {
-                    words.push_back(word);
-                }
-                word.clear();
-            }
-        }
+            cnt++;
     }
+    // Sort the vector by count and lexicographically
+    sort(sortedVector.begin(), sortedVector.end(), compareOccurences);
+    return sortedVector;
 }
 
-/**
- * @brief Count all words and cout them
- *
- * @param words vector of words to count
- */
-void count_and_output(vector<string> words, vector<string>& final, vector<int>& counts)
+vector<Word> separateWords(string &text)
 {
-    sort(words.begin(), words.end());
-
-    // Creates a vector of all words without duplicates
-    for (size_t i = 0; i < words.size(); i++)
+    vector<Word> words;
+    string word;
+    for (auto i = text.begin(); i != text.end(); ++i)
     {
-        if (!count(final.begin(), final.end(), words.at(i)))
+        // Push special characters with a length of 2 bytes
+        if (*i == '\xC3' || *i == '\xC5' || *i == '\xC2')
         {
-            final.push_back(words.at(i));
+            word.push_back(*i);
+            word.push_back(*(i + 1));
+            ++i;
+        }
+        // Ignore special char with a length of 3 bytes
+        else if (*i == '\xE2')
+        {
+            i += 2;
+
+            // If a word was previously composed, push it
+            if (word != "")
+                words.emplace_back(Word(word));
+
+            word.clear();
+        }
+        // Push all normal letters / digits
+        else if (isalnum(*i))
+        {
+            word.push_back(*i);
+        }
+        // All other characters are ignored
+        else
+        {
+            if (word != "")
+            {
+                words.emplace_back(Word(word));
+                word.clear();
+            }
         }
     }
-
-    // Creates a vector with the number of occurences of each word
-    for (size_t i = 0; i < final.size(); ++i)
-    {
-        counts.push_back((int)count(words.begin(), words.end(), final.at(i)));
-    }
-}
-int main(){
-    string text;
-    text = "Tant qu il existera, par le fait des lois et des mœurs, une damnation sociale créant artificiellement, en pleine civilisation, des enfers, et compliquant d une fatalité humaine la destinée qui est divine ; tant que les trois problèmes du siècle, la dégradation de l homme par le prolétariat, la déchéance de la femme par la faim, l atrophie de l enfant par la nuit, ne seront pas résolus ; tant que, dans de certaines régions, l asphyxie sociale sera possible ; en d autres termes, et à un point de vue plus étendu encore, tant qu il y aura sur la terre ignorance et misère, des livres de la nature de celui-ci pourront ne pas être inutiles.";
-    //getline(ifstream("les_miserables.txt"), text, '\0');
-    vector<string> words{};
-    vector<string> final;
-    vector<int> counts;
-    vector<Word> word_vector;
-    put_words_in_vector(words, text);
-    count_and_output(words, final, counts);
-    for(size_t i = 0; i < final.size(); ++i){
-        string word = final.at(i);
-        int count = counts.at(i);
-        word_vector.emplace_back(word, count);
-    }
-
-    //sort(word_vector.begin(), word_vector.end(), Word::get_count);
-    for (size_t i = 0; i < final.size(); ++i)
-        word_vector.at(i).show();
+    return words;
 }
