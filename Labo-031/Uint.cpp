0b098d52eca464a9b9fa34ad956c560d121f09a1
michaelstrefeler <mstrefeler@me.com>
2022-01-14 11:18:49 +0100

    RSA now works with Uint class

--- Labo-031/Uint.cpp
+++ Labo-031/Uint.cpp
@@ -1,38 +1,41 @@
-#include "Uint.hpp";
-#include <iostream>;
+#include "Uint.hpp"
+#include <iostream>
+#include <sstream>
 
 Uint::Uint() {}
 
-Uint::Uint(const std::string &number)
+Uint::Uint(const std::string &s)
 {
-    for (auto it = number.crbegin(); it != number.crend(); ++it)
+    for (auto i = s.crbegin(); i != s.crend(); ++i)
     {
-        if (isdigit(*it))
-            value.push_back(uint32_t(*it) - ASCII_ZERO);
+        if (isdigit(*i))
+            value.push_back(uint32_t(*i) - ASCII_ZERO);
         else
-            throw std::invalid_argument("Not a number");
+            throw std::invalid_argument(
+                "Not a valid number!");
     }
     fixValue();
 }
 
-Uint::Uint(uint64_t unsigned_number) { *this = std::to_string(unsigned_number); }
+bool Uint::isEven() const { return (value.front() % 2) == 0; }
+
+Uint::Uint(uint64_t c) { *this = std::to_string(c); }
 
 Uint &Uint::carry()
 {
-    for (size_t size = 0; size < value.size(); ++size)
+    for (size_t i = 0; i < value.size(); ++i)
     {
-        if (size == value.size() - 1)
+        if (i == value.size() - 1)
         {
-            if (value.at(size) >= 10)
-            {
-                value.push_back(value.at(size) / 10);
-            }
+            if (value.at(i) >= 10)
+                value.push_back(value.at(i) / 10);
         }
         else
-        {
-            value.at(size + 1) += value.at(size) / 10;
-        }
+            value.at(i + 1) += value.at(i) / 10;
+
+        value.at(i) %= 10;
     }
+
     return *this;
 }
 
@@ -40,66 +43,95 @@ int Uint::compare(const Uint &left, const Uint &right) const
 {
     if (left.value.size() == right.value.size())
     {
-        for (auto l = left.value.crbegin(), r = right.value.crbegin(); l != left.value.crend(); ++l, ++r)
+        for (auto l = left.value.crbegin(), r = right.value.crbegin();
+             l != left.value.crend(); ++l, ++r)
         {
             if (*l < *r)
-            {
                 return -1;
-            }
             else if (*l > *r)
-            {
                 return 1;
-            }
         }
         return 0;
     }
     else if (left.value.size() < right.value.size())
-    {
         return -1;
-    }
     return 1;
 }
 
 void Uint::fixValue()
 {
-    while (value.size() > 1 and *(value.end() - 1) == 0)
-    {
+    while (value.size() > 1 && *(value.end() - 1) == 0)
         value.erase((value.end() - 1));
-    }
 }
 
 Uint Uint::division(const Uint &dividend, const Uint &divisor, Uint &remainder)
 {
-    Uint temp(1);
-    Uint temp_divisor = divisor;
-    Uint quotient(0);
+    Uint temp(1), divisorTemp = divisor, quotient(0);
 
-    while (temp_divisor <= divisor)
+    while (divisorTemp <= dividend)
     {
         temp *= 2;
-        temp_divisor *= 2;
+        divisorTemp *= 2;
     }
 
     remainder = dividend;
 
     while (remainder >= divisor)
     {
-        quotient += temp;
-        remainder -= temp_divisor;
+        divisorTemp /= 2;
+        temp /= 2;
+
+        if (remainder >= divisorTemp)
+        {
+            quotient += temp;
+            remainder -= divisorTemp;
+        }
     }
     return quotient;
 }
 
-std::ostream &operator<<(std::ostream &left, const Uint &right)
+size_t Uint::getSize() const { return value.size(); }
+
+Uint mod_pow(Uint base, Uint exp, const Uint &mod)
 {
-    for (auto r = right.value.crbegin(); r != right.value.crend(); ++r)
+    Uint result = 1;
+
+    while (exp > 0)
     {
-        left << *r;
+        if (exp.isEven())
+        {
+            base = base * base % mod;
+            exp /= 2;
+        }
+        else
+        {
+            result = result * base % mod;
+            --exp;
+        }
     }
-    return left;
+    return result;
 }
 
-Uint operator*(uint32_t left, const Uint &right) { return left * right; }
+Uint operator*(uint32_t left, const Uint &right) { return right * left; }
+
+std::ostream &operator<<(std::ostream &out, const Uint &u)
+{
+    for (auto i = u.value.crbegin(); i != u.value.crend(); ++i)
+        out << *i;
+
+    return out;
+}
+
+std::istream &operator>>(std::istream &is, Uint &u)
+{
+    std::string buffer;
+    if (is >> buffer)
+        u = buffer;
+    else
+        is.clear(std::ios::badbit | is.rdstate());
+
+    return is;
+}
 
 Uint Uint::operator+(const Uint &right) const
 {
@@ -122,7 +154,7 @@ Uint Uint::operator*(const Uint &right) const
     return temp;
 }
 
-Uint Uint::operator*(uint32_t &right) const
+Uint Uint::operator*(uint32_t right) const
 {
     Uint temp = *this;
     temp *= right;
@@ -136,7 +168,7 @@ Uint Uint::operator/(const Uint &right) const
     return temp;
 }
 
-Uint Uint::operator/(uint32_t &right) const
+Uint Uint::operator/(uint32_t right) const
 {
     Uint temp = *this;
     temp /= right;
@@ -150,43 +182,39 @@ Uint Uint::operator%(const Uint &right) const
     return temp;
 }
 
-Uint Uint::operator%(uint32_t &right) const
+Uint Uint::operator%(uint32_t right) const
 {
     Uint temp = *this;
     temp %= right;
     return temp;
 }
 
-Uint &Uint::operator++(int dummy)
+Uint Uint::operator++(int)
 {
-    Uint temp = *this;
+    Uint tmp = *this;
     ++(*this);
-    return temp;
+    return tmp;
 }
 
 Uint &Uint::operator++()
 {
     ++value.front();
     if (value.front() > 9)
-    {
         carry();
-    }
     return *this;
 }
 
-Uint &Uint::operator--(int dummy)
+Uint Uint::operator--(int)
 {
-    Uint temp = *this;
+    Uint tmp = *this;
     --(*this);
-    return temp;
+    return tmp;
 }
 
 Uint &Uint::operator--()
 {
     if (value.front() > 1)
-    {
         --value.front();
-    }
     else
     {
         *this -= 1;
@@ -198,15 +226,10 @@ Uint &Uint::operator--()
 Uint &Uint::operator+=(const Uint &right)
 {
     if (value.size() < right.value.size())
-    {
         value.resize(uint32_t(right.value.size()), 0);
-    }
 
     for (size_t i = 0; i < right.value.size(); ++i)
-    {
         value.at(i) += right.value.at(i);
-    }
-
     return carry();
 }
 
@@ -214,14 +237,10 @@ Uint &Uint::operator-=(const Uint &right)
 {
     Uint temp = right;
     if (*this < right)
-    {
-        throw std::invalid_argument("Negative difference not allowed!");
-    }
+        throw std::invalid_argument("Negative difference not permitted");
 
-    if (value.size() > right.value.size())
-    {
+    if (value.size() > temp.value.size())
         temp.value.resize(uint32_t(value.size()), 0);
-    }
 
     for (size_t i = 0; i < value.size(); ++i)
     {
@@ -233,62 +252,53 @@ Uint &Uint::operator-=(const Uint &right)
         value.at(i) -= temp.value.at(i);
     }
     fixValue();
+
     return *this;
 }
 
 Uint &Uint::operator*=(const Uint &right)
 {
     Uint temp = 0;
-
     for (size_t i = 0; i != right.value.size(); i++)
     {
-        temp += right.value.at(i) * (*this);
+        temp += right.value.at(i) * *this;
 
         if (i != right.value.size() - 1)
-        {
             value.insert(value.cbegin(), 0);
-        }
     }
 
     *this = temp;
+
     return *this;
 }
 
-Uint &Uint::operator*=(uint32_t &right)
+Uint &Uint::operator*=(uint32_t right)
 {
     for (uint32_t i = 0; i < value.size(); ++i)
-    {
         value.at(i) *= right;
-    }
-
     return carry();
 }
 
 Uint &Uint::operator/=(const Uint &right)
 {
     if (right == Uint(0))
-    {
-        throw std::runtime_error("Zero division error");
-    }
+        throw std::runtime_error("Zero division error!");
 
     Uint remainder;
     *this = division(*this, right, remainder);
     return *this;
 }
 
-Uint &Uint::operator/=(uint32_t &right)
+Uint &Uint::operator/=(uint32_t right)
 {
     if (right == 0)
-    {
-        throw std::runtime_error("Zero division error");
-    }
+        throw std::runtime_error("Zero division error!");
 
     for (size_t i = value.size() - 1; i > 0; i--)
     {
         value.at(i - 1) += (value.at(i) % right) * 10;
         value.at(i) /= right;
     }
-
     value.at(0) /= right;
     fixValue();
 
@@ -301,14 +311,34 @@ Uint &Uint::operator%=(const Uint &right)
     return *this;
 }
 
-Uint &Uint::operator%=(uint32_t &right)
+Uint &Uint::operator%=(uint32_t right)
 {
     return *this -= (*this / right) * right;
 }
 
-bool Uint::operator<(const Uint &right) const { return compare(*this, right) == -1; };
-bool Uint::operator<=(const Uint &right) const { return compare(*this, right) != 1; };
-bool Uint::operator>(const Uint &right) const { return compare(*this, right) == 1; };
-bool Uint::operator>=(const Uint &right) const { return compare(*this, right) != -1; };
-bool Uint::operator==(const Uint &right) const { return compare(*this, right) == 0; };
-bool Uint::operator!=(const Uint &right) const { return compare(*this, right) != 0; };
+bool Uint::operator<(const Uint &right) const { return compare(*this, right) == -1; }
+
+bool Uint::operator<=(const Uint &right) const { return compare(*this, right) != 1; }
+
+bool Uint::operator>(const Uint &right) const { return compare(*this, right) == 1; }
+
+bool Uint::operator>=(const Uint &right) const { return compare(*this, right) != -1; }
+
+bool Uint::operator==(const Uint &right) const { return compare(*this, right) == 0; }
+
+bool Uint::operator!=(const Uint &right) const { return compare(*this, right) != 0; }
+
+Uint::operator uint64_t() const
+{
+    try
+    {
+        std::stringstream convert;
+        convert << *this;
+        return std::stoull(convert.str());
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << ": Number greater than uint64_t max value" << '\n';
+        return 0;
+    }
+}
--- Labo-031/Uint.hpp
+++ Labo-031/Uint.hpp
@@ -4,52 +4,58 @@
 #include <vector>
 #include <string>
 
-const uint32_t ASCII_ZERO = 48; // ASCII Value of 0
+const uint32_t ASCII_ZERO = 48;
 
-class  Uint{
-    std::vector<uint32_t> value = {};
+class Uint
+{
+private:
+    std::vector<uint32_t> value;
     Uint &carry();
     int compare(const Uint &left, const Uint &right) const;
     void fixValue();
     Uint division(const Uint &dividend, const Uint &divisor, Uint &remainder);
 
-    public:
-    // Constructors
+public:
     Uint();
-    Uint(const std::string &number);
-    Uint(uint64_t unsigned_number);
-    // Friends
-    friend std::ostream &operator<<(std::ostream &left, const Uint &right);
-    friend Uint operator*(uint32_t left, const Uint &right);
+    Uint(const std::string &s);
+    Uint(uint64_t u);
+    bool isEven() const;
+    size_t getSize() const;
 
-    // Arithmetic operator overloads
+    friend Uint mod_pow(Uint base, Uint exp, const Uint &mod);
+    friend class Sint;
+    friend Uint operator*(uint32_t left, const Uint &right);
+    friend std::ostream &operator<<(std::ostream &out, const Uint &u);
+    friend std::istream &operator>>(std::istream &is, Uint &u);
+
     Uint operator+(const Uint &right) const;
     Uint operator-(const Uint &right) const;
     Uint operator*(const Uint &right) const;
-    Uint operator*(uint32_t &right) const;
+    Uint operator*(uint32_t right) const;
     Uint operator/(const Uint &right) const;
-    Uint operator/(uint32_t &right) const;
+    Uint operator/(uint32_t right) const;
     Uint operator%(const Uint &right) const;
-    Uint operator%(uint32_t &right) const;
+    Uint operator%(uint32_t right) const;
+    Uint operator++(int);
     Uint &operator++();
-    Uint &operator++(int dummy);
+    Uint operator--(int);
     Uint &operator--();
-    Uint &operator--(int dummy);
     Uint &operator+=(const Uint &right);
     Uint &operator-=(const Uint &right);
     Uint &operator*=(const Uint &right);
-    Uint &operator*=(uint32_t &right);
+    Uint &operator*=(uint32_t right);
     Uint &operator/=(const Uint &right);
-    Uint &operator/=(uint32_t &right);
+    Uint &operator/=(uint32_t right);
     Uint &operator%=(const Uint &right);
-    Uint &operator%=(uint32_t &right);
+    Uint &operator%=(uint32_t right);
 
-    // Comparison operator overloads
     bool operator<(const Uint &right) const;
     bool operator<=(const Uint &right) const;
     bool operator>(const Uint &right) const;
     bool operator>=(const Uint &right) const;
     bool operator==(const Uint &right) const;
     bool operator!=(const Uint &right) const;
+    explicit operator uint64_t() const;
 };
+
 #endif
